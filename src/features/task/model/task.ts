ea4380e48f2ff70ce0f05724ccc476b505afa583
terbium <67818964+Terbium234@users.noreply.github.com>
2026-05-12 15:00:49 +0900

    Feat/1 task model (#8)
    * chore: task モデルのファイルを作成
    
    * feat: create Task model
    
    ---------
    
    Co-authored-by: tufusa <me@tufusa.net>

---
+++ src/features/task/model/task.ts
@@ -0,0 +1,77 @@
+import { timeStamp } from "console"
+
+class Task{
+    #id : string;
+    #title : string;
+    #description : string;
+    #deadline: Date;
+    #estimated_minutes: number;
+    #actual_minutes: number;
+    #priority: number;
+    #progress: number;
+    #status: string;
+
+    constructor(
+        id : string,
+        title : string,
+        description : string,
+        deadline: Date,
+        estimated_minutes: number,
+        actual_minutes: number,
+        priority: number,
+        progress: number,
+        status: string,
+    ) {
+        this.#id=id 
+        this.#title = title
+        this.#description = description
+        this.#deadline = deadline
+        this.#estimated_minutes = estimated_minutes
+        this.#actual_minutes = actual_minutes
+        this.#priority = priority
+        this.#progress = progress
+        this.#status = status
+
+
+        
+
+    }
+
+    
+    getId(): string {
+        return this.#id;
+    }
+
+    getTitle(): string {
+        return this.#title;
+    }
+
+    getDescription(): string {
+        return this.#description;
+    }
+
+    getDeadline(): Date {
+        return this.#deadline;
+    }
+
+    getEstimatedMinutes(): number {
+        return this.#estimated_minutes;
+    }
+
+    getActualMinutes(): number {
+        return this.#actual_minutes;
+    }
+
+    getPriority(): number {
+        return this.#priority;
+    }
+
+    getProgress(): number {
+        return this.#progress;
+    }
+
+    getStatus(): string {
+        return this.#status;
+    }
+
+}
