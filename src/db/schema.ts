3cb49f301fcba4f7fd5f3d9899a7986e591121cc
tufusa <me@tufusa.net>
2026-05-12 16:43:18 +0900

    feat: スキーマを定義

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,25 @@
-// import { sql } from "drizzle-orm";
-// import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-// export const todos = sqliteTable("todos", {
-//   id: integer({ mode: "number" }).primaryKey({
-//     autoIncrement: true,
-//   }),
-//   title: text().notNull(),
-//   createdAt: integer("created_at", { mode: "timestamp" }).default(
-//     sql`(unixepoch())`,
-//   ),
-// });
+export const event = sqliteTable("event", {
+  id: text().primaryKey(),
+  title: text().notNull(),
+  description: text(),
+  startAt: integer("start_at", { mode: "timestamp" }).notNull(),
+  endAt: integer("end_at", { mode: "timestamp" }).notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" }).default(
+    sql`(unixepoch())`,
+  ),
+});
+
+export const task = sqliteTable("task", {
+  id: text().primaryKey(),
+  title: text().notNull(),
+  description: text(),
+  deadline: integer({ mode: "timestamp" }).notNull(),
+  estimatedMinutes: integer().notNull(),
+  actualMinutes: integer().default(0),
+  priority: integer(),
+  progress: integer(),
+  status: text(),
+});
