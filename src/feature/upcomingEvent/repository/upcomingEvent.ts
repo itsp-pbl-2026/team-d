0d575d9889779470f109f7a5279c47adb9371b25
Otsuka Ken <otsuka.k.a2d8@m.isct.ac.jp>
2026-05-12 16:10:16 +0900

    リポジトリーのインターフェイスを追加したでやんす。

---
+++ src/feature/upcomingEvent/repository/upcomingEvent.ts
@@ -0,0 +1,7 @@
+import type { UpcomingEvent } from "../model/upcomingEvent";
+
+export interface UpcomingEventRepository {
+  save(event: UpcomingEvent): Promise<void>;
+  findById(id: string): Promise<UpcomingEvent | undefined>;
+  findAll(): Promise<UpcomingEvent[]>;
+}
