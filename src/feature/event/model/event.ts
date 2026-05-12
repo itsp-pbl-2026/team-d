0b175ca95d55e32f2b1f207b80bee583c634df76
Latif <156067272+Snaif-Yamachan@users.noreply.github.com>
2026-05-12 15:26:32 +0900

    feat: Event モデルを作成 (#7)
    * get関数を含むeventクラスの作成
    
    * format
    
    * Update src/feature/event/model/event.ts
    
    Co-authored-by: tufusa <me@tufusa.net>
    
    * Update src/feature/event/model/event.ts
    
    Co-authored-by: tufusa <me@tufusa.net>
    
    * apply review
    
    * apply review
    
    ---------
    
    Co-authored-by: tufusa <me@tufusa.net>

---
+++ src/feature/event/model/event.ts
@@ -0,0 +1,36 @@
+export class UpcomingEvent {
+  #id: string;
+  #title: string;
+  #description: string;
+  #startAt: Date;
+  #endAt: Date;
+
+  constructor(
+    id: string,
+    title: string,
+    description: string,
+    startAt: Date,
+    endAt: Date,
+  ) {
+    this.#id = id;
+    this.#title = title;
+    this.#description = description;
+    this.#startAt = startAt;
+    this.#endAt = endAt;
+  }
+  getId(): string {
+    return this.#id;
+  }
+  getTitle(): string {
+    return this.#title;
+  }
+  getDescription(): string {
+    return this.#description;
+  }
+  getStartAt(): Date {
+    return this.#startAt;
+  }
+  getEndAt(): Date {
+    return this.#endAt;
+  }
+}
