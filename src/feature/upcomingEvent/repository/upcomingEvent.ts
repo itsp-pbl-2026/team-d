8a5b7f13acc74fa8e4f5ab40e4030f51af31fe02
Otsuka Ken <otsuka.k.a2d8@m.isct.ac.jp>
2026-05-19 14:07:29 +0900

    やんす。

--- src/feature/upcomingEvent/repository/upcomingEvent.ts
+++ src/feature/upcomingEvent/repository/upcomingEvent.ts
@@ -4,4 +4,5 @@ export interface UpcomingEventRepository {
   save(event: UpcomingEvent): Promise<void>;
   findById(id: string): Promise<UpcomingEvent | undefined>;
   findAll(): Promise<UpcomingEvent[]>;
+  delete(id: string): Promise<void>;
 }
