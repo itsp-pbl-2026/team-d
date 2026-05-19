4bf56cfa460e1af436a4edaf0caf8e532d0c10a9
Terbium234 <cungangte485@gmail.com>
2026-05-19 18:11:04 +0900

    feat: add test

--- src/features/task/service/get.ts
+++ src/features/task/service/get.ts
@@ -1,25 +1,25 @@
 import type { Task } from "../model/task";
-import type { TaskRepository } from "../repository/task"
+import type { TaskRepository } from "../repository/task";
 
 export class GetTaskService {
- #taskRepository: TaskRepository;
+  #taskRepository: TaskRepository;
 
- constructor(taskRepository: TaskRepository) {
-  this.#taskRepository = taskRepository;
- }
-
- async getById(id: string): Promise<Task> {
-  const task = await this.#taskRepository.findById(id);
-  if (task == null) {
-   // TODO: Result型を作る?
-   throw new Error("No event found.");
+  constructor(taskRepository: TaskRepository) {
+    this.#taskRepository = taskRepository;
   }
 
-  return task;
- }
+  async getById(id: string): Promise<Task> {
+    const task = await this.#taskRepository.findById(id);
+    if (task == null) {
+      // TODO: Result型を作る?
+      throw new Error("No Task found.");
+    }
 
- async getAll(): Promise<Task[]> {
-  const tasks = await this.#taskRepository.findAll();
-  return tasks;
- }
+    return task;
+  }
+
+  async getAll(): Promise<Task[]> {
+    const tasks = await this.#taskRepository.findAll();
+    return tasks;
+  }
 }
