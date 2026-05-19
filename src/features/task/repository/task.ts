970a2fc3c8dc842997ba0ee327efd0716178fc56
Terbium234 <cungangte485@gmail.com>
2026-05-19 14:29:02 +0900

    feat: implement save function

--- src/features/task/repository/task.ts
+++ src/features/task/repository/task.ts
@@ -1,4 +1,6 @@
 import type { Task } from "../model/task";
+import {drizzleClient} from "../../../db/drizzleClient"
+import {task} from "../../../db/schema"
 
 export interface TaskRepository {
   findById(id: string): Promise<Task | undefined>;
@@ -6,4 +8,18 @@ export interface TaskRepository {
 
   save(task: Task): Promise<void>;
   delete(id: string): Promise<void>;
+}
+
+async function save(task_:Task){
+  await drizzleClient.insert(task).values({
+    id: task_.getId(),
+    title: task_.getTitle(),
+    description: task_.getDescription(),
+    deadline: task_.getDeadline(),                // Drizzleが自動的にDateオブジェクトをSQLiteに適した形式に変換してくれます
+    estimatedMinutes: task_.getEstimatedMinutes(),
+    actualMinutes: task_.getActualMinutes(),
+    priority: task_.getPriority(),
+    progress: task_.getProgress(),
+    status: task_.getStatus(),
+  })
 }
