d1b02df5f6afe97b99ff34399f20fe4d8889a3e5
tufusa <me@tufusa.net>
2026-05-20 15:17:34 +0900

    chore: テスト用DBを生成する関数を追加

--- src/db/drizzleClient.ts
+++ src/db/drizzleClient.ts
@@ -2,3 +2,8 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import * as schema from "./schema.ts";
 
 export const drizzleClient = drizzle(process.env.DATABASE_URL, { schema });
+
+export const createTestDrizzleClient = () => drizzle(":memory:", { schema });
+export type DrizzleClient = typeof drizzleClient;
+
+export { schema };
