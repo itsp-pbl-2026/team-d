41cc0a148ca44e5edd814b02237e2e3a7ee00d9e
Snaif-Yamachan <mattu.1578@gmail.com>
2026-06-16 15:34:24 +0900

    ScheduleのSchemaを定義しました

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,6 +2,7 @@ import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import type { TaskId } from "#/features/task/model/task";
 import type { UpcomingEventId } from "#/features/upcomingEvent/model/upcomingEvent";
+import type { ScheduleId } from "#/features/schedule/model/schedule";
 
 export const event = sqliteTable("event", {
   id: text().$type<UpcomingEventId>().primaryKey(),
@@ -27,4 +28,12 @@ export const task = sqliteTable("task", {
   createdAt: integer("created_at", { mode: "timestamp" }).default(
     sql`(unixepoch())`,
   ),
+});
+
+export const schedule = sqliteTable("schedule", {
+  id: text().$type<ScheduleId>().primaryKey(),
+  title: text().notNull(),
+  startAt: integer("start_at", { mode: "timestamp" }).notNull(),
+  endAt: integer("end_at", { mode: "timestamp" }).notNull(),
+  taskId: text("task_id").$type<TaskId>().notNull().references(() => task.id),
 });
