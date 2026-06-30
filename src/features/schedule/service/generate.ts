d684e2e54b74d71f6630ef1367bf0dfab5a2c853
Motonari Yamanaka <mymnk1234@gmail.com>
2026-06-30 14:53:43 +0900

    feat: GenerateScheduleService を実装する (#70)
    * feat: GenerateScheduleService を実装する
    
    * feat: GenerateScheduleService を実装する
    
    * feat: GenerateScheduleService を実装する

---
+++ src/features/schedule/service/generate.ts
@@ -0,0 +1,93 @@
+import { idGenerator } from "#/features/id";
+import type { Task } from "#/features/task/model/task";
+import type { TaskRepository } from "#/features/task/repository/task";
+import type { UpcomingEvent } from "#/features/upcomingEvent/model/upcomingEvent";
+import type { UpcomingEventRepository } from "#/features/upcomingEvent/repository/upcomingEvent";
+import { Schedule } from "../model/schedule";
+import type { ScheduleRepository } from "../repository/schedule";
+import type {
+  GenerateScheduleDomainService,
+  GenerateScheduleDomainServiceInput,
+} from "./generateDomainService";
+
+const serializeTask = (
+  task: Task,
+): GenerateScheduleDomainServiceInput["tasks"][number] => ({
+  id: task.getId(),
+  title: task.getTitle(),
+  description: task.getDescription(),
+  deadline: task.getDeadline(),
+  estimatedMinutes: task.getEstimatedMinutes(),
+  actualMinutes: task.getActualMinutes(),
+  priority: task.getPriority(),
+  progress: task.getProgress(),
+  status: task.getStatus(),
+});
+
+const serializeUpcomingEvent = (
+  upcomingEvent: UpcomingEvent,
+): GenerateScheduleDomainServiceInput["events"][number] => ({
+  id: upcomingEvent.getId(),
+  title: upcomingEvent.getTitle(),
+  description: upcomingEvent.getDescription(),
+  startAt: upcomingEvent.getStartAt(),
+  endAt: upcomingEvent.getEndAt(),
+});
+
+export class GenerateScheduleService {
+  #taskRepository: TaskRepository;
+  #upcomingEventRepository: UpcomingEventRepository;
+  #scheduleRepository: ScheduleRepository;
+  #generateScheduleDomainService: GenerateScheduleDomainService;
+
+  constructor(
+    taskRepository: TaskRepository,
+    upcomingEventRepository: UpcomingEventRepository,
+    scheduleRepository: ScheduleRepository,
+    generateScheduleDomainService: GenerateScheduleDomainService,
+  ) {
+    this.#taskRepository = taskRepository;
+    this.#upcomingEventRepository = upcomingEventRepository;
+    this.#scheduleRepository = scheduleRepository;
+    this.#generateScheduleDomainService = generateScheduleDomainService;
+  }
+
+  async handle(): Promise<Schedule[]> {
+    const [tasks, upcomingEvents] = await Promise.all([
+      this.#taskRepository.findAll(),
+      this.#upcomingEventRepository.findAll(),
+    ]);
+
+    const generatedSchedules = await this.#generateScheduleDomainService.handle(
+      {
+        tasks: tasks.map(serializeTask),
+        events: upcomingEvents.map(serializeUpcomingEvent),
+      },
+    );
+
+    const tasksById = new Map<string, Task>(
+      tasks.map((task) => [task.getId(), task]),
+    );
+
+    const schedules = generatedSchedules.map((generatedSchedule) => {
+      const task = tasksById.get(generatedSchedule.taskId);
+      if (task == null) {
+        throw new Error(`No task found: ${generatedSchedule.taskId}`);
+      }
+
+      return new Schedule(
+        idGenerator.generate<Schedule>(),
+        task.getTitle(),
+        generatedSchedule.startAt,
+        generatedSchedule.endAt,
+        task,
+      );
+    });
+
+    await Promise.all(
+      schedules.map((schedule) => this.#scheduleRepository.save(schedule)),
+    );
+
+    return schedules;
+  }
+}
